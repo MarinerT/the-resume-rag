11c37b94f887021d2b2e7c130c52af02f10b0583
Todd Marino <marino.todd@gmail.com>
2024-02-05 10:15:15 -0500

    updated load script and utils

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,4 +1,15 @@
-from toddbo import connect_to_chroma, load_documents_to_chroma
+import streamlit as st
+from toddbo import connect_to_chroma, unzip, fetch_load_split, connect_to_collection, upload_to_collection
 
-client = connect_to_chroma()
-load_documents_to_chroma(client)
+# Load data to Chroma
+
+if __name__ == '__main__':
+    print('Loading data to Chroma...')
+
+    client = connect_to_chroma(chroma_host=st.secrets.chroma.CHROMA_HOST,
+                               chroma_port=st.secrets.chroma.CHROMA_PORT
+                               )
+    unzip()
+    documents = fetch_load_split()
+    collection = connect_to_collection(client, st.secrets.chroma.COLLECTION)
+    upload_to_collection(documents, collection)
